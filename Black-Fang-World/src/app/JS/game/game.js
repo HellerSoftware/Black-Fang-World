a9fd1d26a52cc02e1b250a475eab31132c0d78c4
Jorge <hellersoftware229@gmail.com>
2021-03-24 16:26:58 +0100

    eliminacion de errores y mejora de algunas funciones

---
+++ Black-Fang-World/src/app/JS/game/game.js
@@ -0,0 +1,48 @@
+function startGame(playerName){
+
+    var Map1Code = fs.readFileSync('c:/HellerGames/BFW/maps/surface/map1/x1y1/map.bfwmap', 'utf8');
+    var map1Y = Map1Code.split('\n');
+    var map1 = [];
+    for(var i = 0; i < map1Y.length; i++){
+        var map = map1Y[i];
+        for(var j = 0; j < map1Y[i].length; j++){
+        map1[i] = map.split('-');
+        }
+    }
+
+    appUI.innerHTML = '<div id="el" style="float: right;"></div>';
+
+    var mapElements = document.getElementById('el');
+    
+    var textureSizeH = (windowResolution[1] / 25);
+    var textureSizeW = (windowResolution[0] / 25);
+
+    function mapCreate(m, y, x){
+    
+        tsh = textureSizeH;
+        
+        if(m[y][x] == 'ground'){
+            mapElements.innerHTML += '<div class="ground" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/ground.png' + ');"></div>';
+        }
+        if(m[y][x] == 'stone_'){
+            mapElements.innerHTML += '<div class="stone" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/stone.png' + ');"></div>';
+        }
+        if(m[y][x] == 'water_'){
+            mapElements.innerHTML += '<div class="water" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/water.png' + ');"></div>';
+        }
+        if(m[y][x] == 'none__'){
+            mapElements.innerHTML += '<div class="none" style="width: ' + tsh + 'px; height: ' + tsh + 'px;"></div>';
+        }
+    }
+    
+    console.log(map1)
+    for(var i = 0 ; i < map1.length; i++) {
+        for(var j = 0; j < map1[i].length; j++) {
+            mapCreate(map1, i, j);
+        }
+    }
+}
+
+module.exports = {
+    startGame
+}
--- Black-Fang-World/src/app/JS/index.js
+++ Black-Fang-World/src/app/JS/index.js
@@ -10,6 +10,8 @@ const {gameObject, dc, encoderReturn, decoderReturn, function_registration_and_l
 //funciones relacionadas con la UI, son usados por gameObject
 const {viewChange, viewReturn} = require('../modules/UI_modules/ui_functions');
 
+const {startGame} = require('../JS/game/game');
+
 //variable que recoge el elemnto sustituible de la interfaz grafica
 var appUI = document.getElementById('AppUI');
 //
@@ -23,15 +25,7 @@ var allWorld = [
     ['',            'q', 'b    ', 'v']
 ];
 
-var Map1Code = fs.readFileSync('c:/HellerGames/BFW/maps/surface/map1/x1y1/map.bfwmap', 'utf8');
-var map1Y = Map1Code.split('\n');
-var map1 = [];
-for(var i = 0; i < map1Y.length; i++){
-    var map = map1Y[i];
-    for(var j = 0; j < map1Y[i].length; j++){
-    map1[i] = map.split('-');
-    }
-}
+
 //
 
 //
@@ -56,50 +50,11 @@ else{
 }
 //
 
-/*for(var i = 0; i < 25; i++){
-    //console.log(mapX[0][0])
-    document.write(map1[3][35] + '<br>');
-    //document.write('var y' + i + '= []; <br>')
-}*/
-
-function tamVentana(){var tam = [0, 0];if (typeof window.innerWidth != 'undefined'){tam = [window.innerWidth,window.innerHeight];}else if (typeof document.documentElement != 'undefined'&& typeof document.documentElement.clientWidth !='undefined' && document.documentElement.clientWidth != 0){tam = [document.documentElement.clientWidth,document.documentElement.clientHeight];}else{tam = [document.getElementsByTagName('body')[0].clientWidth,document.getElementsByTagName('body')[0].clientHeight];}return tam;}
+function winResolutionReturn(){var res = [0, 0];if(typeof window.innerWidth != 'undefined'){res = [window.innerWidth,window.innerHeight];}else if(typeof document.documentElement != 'undefined'&& typeof document.documentElement.clientWidth !='undefined' && document.documentElement.clientWidth != 0){res = [document.documentElement.clientWidth,document.documentElement.clientHeight];}else{res = [document.getElementsByTagName('body')[0].clientWidth,document.getElementsByTagName('body')[0].clientHeight];}return res;}
 
 
 var heightScreenResolution = screen.height;
-var WindowResolution = tamVentana();
-var textureSizeH = (WindowResolution[1] / 25);
-var textureSizeW = (WindowResolution[0] / 25);
-console.log(tamVentana());
-appUI.innerHTML = '<div id="el" style="text-align: center;"></div>';// style="float: right";
-var mapElements = document.getElementById('el');
-function mapCreate(m, y, x){
-
-    tsh = textureSizeH;
-    
-    if(m[y][x] == 'ground'){
-        mapElements.innerHTML += '<div class="ground" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/ground.png' + ');"></div>';
-    }
-    if(m[y][x] == 'stone_'){
-        mapElements.innerHTML += '<div class="stone" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/stone.png' + ');"></div>';
-    }
-    if(m[y][x] == 'water_'){
-        mapElements.innerHTML += '<div class="water" style="width: ' + tsh + 'px; height: ' + tsh + 'px; background-image:url(' + 'c:/HellerGames/BFW/textures/water.png' + ');"></div>';
-    }
-    if(m[y][x] == 'none__'){
-        mapElements.innerHTML += '<div class="none" style="width: ' + tsh + 'px; height: ' + tsh + 'px;"></div>';
-    }
-}
-
-console.log(map1)
-for(var i = 0 ; i < map1.length; i++) {
-    for(var j = 0; j < map1[i].length; j++) {
-        mapCreate(map1, i, j);
-    }
-}
-
-/*
-for(var i = 0; i < 25; i++){
-    console.log(mapX[i])
-    document.write(mapY[0] + '<br>');
-    //document.write('var y' + i + '= []; <br>')
-}*/
+var heightScreenResolution = screen.height;
+var windowResolution = winResolutionReturn();
+console.log('Resolucion de la pantalla: ' + windowResolution[0] + ' x ' + windowResolution[1]);
+startGame();
