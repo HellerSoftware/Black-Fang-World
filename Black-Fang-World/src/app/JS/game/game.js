a75f18638f7accc0c1ea75e8191ab566558f0795
Jorge <hellersoftware229@gmail.com>
2021-03-29 21:12:23 +0200

    movimiento del personaje

--- Black-Fang-World/src/app/JS/game/game.js
+++ Black-Fang-World/src/app/JS/game/game.js
@@ -43,6 +43,79 @@ function startGame(playerName){
             mapCreate(idN, map1, i, j);
         }
     }
+
+
+
+    var xp = 5;
+    var yp = 5;
+    var lastMovementDirection;
+    var mainGamePositionID = yp + '-' + xp;
+    var currentPosition = document.getElementById(mainGamePositionID);
+    psh = textureSizeH - 15;
+    currentPosition.innerHTML = '<div style="font-size: ' + psh + 'px;">🧸</div>';
+    function mp(y, x, direction){
+        var tsh = textureSizeH - 5;
+        var mainCurrentGamePositionID = yp + '-' + xp;
+        lastMovementDirection = direction;
+        if(direction == 'u'){
+            y-=1;
+            x+=0;
+        }
+        if(direction == 'd'){
+            y+=1;
+            x+=0;
+        }
+        if(direction == 'r'){
+            y+=0;
+            x+=1;
+        }
+        if(direction == 'l'){
+            y+=0;
+            x-=1;
+        }
+        var fxp = x;
+        var fyp = y;
+        var futurePositionID = y + '-' + x;
+        //var currentPositionElement = document.getElementById('3-5').getAttribute('element-type');
+        var currentPosition = document.getElementById(mainCurrentGamePositionID);
+        var futurePositionElement = document.getElementById(futurePositionID).getAttribute('element-type');
+        var futurePosition = document.getElementById(futurePositionID);
+        if(futurePositionElement == 'g1'){
+            console.log('puedes pasar');
+            currentPosition.innerHTML = '';
+            futurePosition.innerHTML = '<div style="font-size: ' + psh + 'px;">🧸</div>';
+            xp = fxp;
+            yp = fyp;
+        }
+        else{
+            console.log('no puedes pasar');
+        }
+    }
+    document.querySelector('#u').addEventListener('click', () =>{mp(yp, xp, "u");})
+    document.querySelector('#r').addEventListener('click', () =>{mp(yp, xp, "r");})
+    document.querySelector('#l').addEventListener('click', () =>{mp(yp, xp, "l");})
+    document.querySelector('#d').addEventListener('click', () =>{mp(yp, xp, "d");})
+    document.querySelector('#j').addEventListener('click', () =>{
+        //p = posibility
+        if(lastMovementDirection == 'u'){
+            var jumpDestination = yp - 1;
+            mp(jumpDestination, xp, "u");
+        }
+        if(lastMovementDirection == 'r'){
+            var jumpDestination = xp + 1;
+            mp(yp, jumpDestination, "r");
+        }
+        if(lastMovementDirection == 'l'){
+            var jumpDestination = xp - 1;
+            mp(yp, jumpDestination, "l");
+        }
+        if(lastMovementDirection == 'd'){
+            var jumpDestination = yp + 1;
+            mp(jumpDestination, xp, "d");
+        }
+
+        //usar el salto continuamente tiene 2 penalizaciones, por un lado gastas mas energia y por otro tine un temporizador que hace que no puedas spamear salto, ese temporizador puede tardar entre 5 y 2 segundos dependiendo de tu nivel de fuerza
+    })
 }
 
 module.exports = {
